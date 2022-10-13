12921dd2e91aa818ce426b6eec018536f2aea2e9
Felipe <felipesullivan50@gmail.com>
2022-10-12 22:19:03 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = 'design-engineer-system-project'
+    }
+
+    return config
   }
 }
